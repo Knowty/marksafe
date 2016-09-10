1209693a1564f984b4b26d205ff8c61e387ac6ee
abhinav <abhinav.i@knowlarity.com>
2016-09-10 16:50:11 +0530

    Can now add refugees

--- refugee/forms.py
+++ refugee/forms.py
@@ -2,12 +2,3 @@ from django.forms import ModelForm
 
 from refugee.models import Refugee
 
-
-class RefugeeCreateForm(ModelForm):
-    """
-    Used to submit details about a refugee.
-    """
-
-    class Meta:
-        model = Refugee
-        exclude = ['safety_level']
--- refugee/urls.py
+++ refugee/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-
+from django.views.generic import TemplateView
 from refugee.views import RefugeeCreateView
 
 urlpatterns = [
-    url(r'^create$', RefugeeCreateView.as_view())
+    url(r'^create$', RefugeeCreateView.as_view()),
+    url(r'^success$', TemplateView.as_view(template_name='refugee/created_successfully.html'))
 ]
