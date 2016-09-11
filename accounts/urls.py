602deefdc7c11479c6c872b0462f55b151345ef2
naren <naren.yellavula@knowlarity.com>
2016-09-11 10:39:22 +0530

    Added signup(email/password) for the web app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from accounts.forms import LoginForm
-from accounts.views import Home
+from accounts.views import Signup
 from django.contrib.auth import views
 from victim.views import VictimSearchView
 
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^login/', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}),
     url(r'^logout/$', 'django.contrib.auth.views.logout',
                           {'next_page': '/'}),
+    url(r'^signup/$', Signup.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,29 @@
-from django.contrib.auth import get_user_model
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from accounts.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
-class LoginRequiredMixin(object):
-    @classmethod
-    def as_view(cls, **initkwargs):
-        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
-        return login_required(view)
-
-# Create your views here.
-
-class Home(LoginRequiredMixin, View):
-    template_name = "home.html"
-    login_url = 'login/'
+class Signup(View):
+    template_name = "signup.html"
     def __init__(self, **kwargs):
         pass
 
     def get(self, request):
         # Only fetch students
-        return render(request, self.template_name, {'users': {}, 'tasks': {}})
+        return render(request, self.template_name)
+    
+    def post(self, request):
+        try:
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            email = request.POST.get("email")
+            raw_password = request.POST.get("password")
+            new_user = User.objects.create_user(email, first_name= first_name, last_name = last_name, email = email)
+            new_user.set_password(raw_password)
+            new_user.save()
+            login(self.request, new_user)
+        except:
+            pass
+        return redirect('/')
